f8304b17dedf8aeeb5e3e5e9c1cefe4816316e71
Vinicius <vinicardoso216@gmail.com>
2022-12-15 21:00:05 -0300

    refactor: adding factory pattern to tests

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,19 +1,14 @@
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "./cancel-notification";
-import { Notification } from "@application/entities/notification";
-import { Content } from "@application/entities/content";
 import { NotificationNotFound } from "./errors/notification-not-found";
+import { makeNotification } from "@test/factories/notification-factory";
 
 describe('Cancel notification', () => {
     it('should be able to cancel a notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
         const cancelNotification = new CancelNotification(notificationsRepository);
 
-        const notification = new Notification({
-            category: 'social',
-            content: new Content('New friendship request'),
-            recipientId: 'example-recipient-id'
-        });
+        const notification = makeNotification();
 
         await notificationsRepository.create(notification);
 
